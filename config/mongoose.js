5f753736ea544798024d77e56b7a7774ddc378ae
GANLu <ganlug@stu.xjtu.edu.cn>
2016-09-01 23:21:28 +0800

    added new require configurations for models

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,5 +3,8 @@ var config = require('./config.js');
 
 module.exports = function() {
     var db = mongoose.connect(config.mongodb);
+
+    require('../models/hierarchy.js');
+    require('../models/plant.js');
     return db;
 };
