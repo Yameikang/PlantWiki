0cf4bd10b645eb6a2251e2706a36ed4093c95db8
GANLu <ganlug@stu.xjtu.edu.cn>
2016-09-01 23:32:09 +0800

    added scripts to test for mongodb

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,40 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var request = require('superagent');
 
+//mongodb model for storing plant information
+var mongoose = require('mongoose');
+var Plant = mongoose.model('Plant');
+
+
+
 var url = 'http://frps.eflora.cn/v/1';
+//url = 'http://frps.eflora.cn/name/a';
+
 var links = [], wiki = [];
+
+//test
+router.get('/test', function(req, res, next){
+    var plant = new Plant({
+        fullname: "test",
+        appearanceDesp: "test",
+        description: "test"
+    });
+
+    plant.save(function(err){
+        if(err){
+            res.end("Error");
+            return next();
+        }
+
+        Plant.find({}, function(err, docs){
+            if(err){
+                res.end("Error");
+                return next();
+            }
+            res.json(docs);
+        });
+    });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,10 +47,11 @@ router.get('/', function(req, res, next) {
         }else{
           var $=cheerio.load(res.text);
 
-          var data = $('div.divmain');
-          console.log(data.html());
+          //var data = $('div.divmain');
+          //console.log(data.html());
 
           var linkList = $('div[style="float:left;margin-right:5px;"] a');
+           // var linkList = [];
           linkList.map(function(i, link){
             links.push('http://frps.eflora.cn/getfam.ashx?t='+$(link).attr('href').substring(3));
 
@@ -26,9 +59,18 @@ router.get('/', function(req, res, next) {
                 .end(function(error, response){
                   if(error) console.error(error);
                   else{
+                      //console.log(link);
                     var $$ = cheerio.load(response.text);
+                      var data = [];
+                      $$('a').each(function(i,elem){
+                          data[i] = $$(this).attr('href');
+                      });
+                      console.log(data.toString());
                     //console.log($$.html());
                       //wiki.push();
+                      //var b = $$('a[style="color:#000000"]').attr('href');
+                      //var p = $$('a[style="color:#000000"]').html();
+                      //console.log(b + p +'\n');
                   }
                 });
           });
@@ -36,10 +78,48 @@ router.get('/', function(req, res, next) {
           console.log(links);
 
            //getIAjaxUrlList(20);
+          //getInfoFromLinks(links);
         }
       });
 
-  res.send('respond with a resource');
+  res.send('New resources');
 });
 
+var getInfoFromLinks = function(links){
+  console.log(links.length);
+  var tmp = 0;
+  for(tmp; tmp < links.length; tmp++) {
+    //console.log(links[tmp]);
+    (function(e) {
+        //console.log(e);
+      request.get(links[e])
+          .end(function (error, response) {
+            if (error) ;//console.error(error);
+            else {
+              //console.log(links[e]);
+              var $ = cheerio.load(response.text);
+               // console.log($.html()+'\n');
+              var b = $('a[style="color:#000000"]').attr('href');
+                var p = $('a[style="color:#000000"]').html();
+              console.log(b + p);
+
+
+              //var t = $('a').attr('href');
+              //console.log(t.toString());
+              /*request.get('http://frps.eflora.cn' + t.toString())
+               .end(function (err, res) {
+               if (err) console.error(err);
+               else {
+               var $$$ = cheerio.load(res.text);
+               //console.log($$$.html());
+               //console.log(data.toString());
+               }
+               })*/
+            }
+          });
+    })(tmp);
+  }
+};
+
 module.exports = router;
+
