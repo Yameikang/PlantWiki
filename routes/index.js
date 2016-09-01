330f655c851ce86546475a0b8eae897e34fa8f88
GANLu <ganlug@stu.xjtu.edu.cn>
2016-09-01 22:24:18 +0800

    new complete links from pages

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require('superagent');
+var cheerio = require('cheerio');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  request.get('http://frps.eflora.cn/getfam.ashx?t=3(1)')
+      .end(function(error, response){
+        if(error) console.error(error);
+        else{
+          var $ = cheerio.load(response.text);
+          //console.log($.html());
+
+            var data = [];
+            $('a').each(function(i, elem){
+                data[i] = $(this).attr('href');
+            });
+
+            console.log(data.toString());
+
+
+            /*request.get('http://frps.eflora.cn'+data.toString())
+                .end(function(err, res){
+                    if(err) console.error(err);
+                    else{
+                        var $$$ = cheerio.load(res.text);
+                        //console.log($$$.html());
+                    }
+                })*/
+        }
+      });
+
   res.render('index', { title: 'Express' });
 });
 
