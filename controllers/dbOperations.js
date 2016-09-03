1bce6529c729adadfce83e60cfa6813942d1ae5e
GANLu <ganlug@stu.xjtu.edu.cn>
2016-09-03 19:49:21 +0800

    basic mongodb operations (insert, update, find ...)

--- controllers/dbOperations.js
+++ controllers/dbOperations.js
@@ -1,67 +1,58 @@
-var mongoose = require('mongoose'),
-    cheerio = require('cheerio'),
-    request = require('superagent'),
-    Plant = mongoose.model('Plant'),
-    Hierarchy = mongoose.model('Hierarchy');
-
+//var mongoose = require('mongoose');
 var assert = require('assert');
 
-var url = 'http://frps.eflora.cn/v/1';
-var url1 = 'http://frps.eflora.cn/name/a';
 
-var links = [], wiki = [];
-
-exports.insertDocument = function(db, document, collection, callback) {
+exports.insertDocument = function(document, collection, callback) {
     // Get the documents collection
-    var coll = db.collection(collection);
+    //var coll = db.collection(collection);
     // Insert some documents
-    coll.insert(document, function(err, result){
+    collection.insert(document, function(err, result){
         assert.equal(err, null);
         console.log("Inserted "+result.result.n + " documents into the document collection " + collection);
         callback(result);
     });
 };
 
-exports.findDocuments = function(db, collection, callback){
+exports.findDocuments = function(collection, callback){
     // Get the documents collection
-    var coll = db.collection(collection);
+    //var coll = db.collection(collection);
 
     // Find some documents
-    coll.find({}).toArray(function(err, docs){
+    collection.find({}).toArray(function(err, docs){
         assert.equal(err, null);
         callback(docs);
     });
 };
 
-exports.findDocumentsByCondition = function(db, condition,collection, callback){
+exports.findDocumentsByCondition = function(condition,collection, callback){
     // Get the documents collection
-    var coll = db.collection(collection);
+    //var coll = db.collection(collection);
 
     //Find some documents by condition
-    coll.find(condition).toArray(function(err, docs){
+    collection.find(condition).toArray(function(err, docs){
         assert.equal(err, null);
         callback(docs);
     });
 };
 
-exports.removeDocument = function(db, document, collection,callback){
+exports.removeDocument = function(document, collection,callback){
     // Get the documents collection
-    var coll = db. collection(collection);
+    //var coll = db. collection(collection);
 
     // Delete the document
-    coll.deleteOne(document, function(err, result){
+    collection.deleteOne(document, function(err, result){
         assert.equal(err, null);
         console.log("Removed the document "+ document);
         callback(result);
     });
 };
 
-exports.updateDocument = function(db, document, update, collection, callback){
+exports.updateDocument = function(document, update, collection, callback){
     // Get the documents collection
-    var coll = db.collection(collection);
+    //var coll = db.collection(collection);
 
     // Update document
-    coll.updateOne(document,
+    collection.updateOne(document,
         {$set:update}, null, function(err, result){
             assert.equal(err, null);
             console.log("Updated the document with "+ update);
